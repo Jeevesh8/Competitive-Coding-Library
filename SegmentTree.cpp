a170acb697d901344e50b86c2029e75140bb3e9f
jeevesh8 <creativityinczenyoga@gmail.com>
2019-09-07 22:38:48 +0530

    Debugged Code with working example

--- SegmentTree.cpp
+++ SegmentTree.cpp
@@ -44,29 +44,28 @@ private :
     
     SegmentTreeNode* root;
 
-    RetTypeOfCalc BuildTreeNow(SegmentTreeNode* root, size_t i=1, size_t start_idx=beg, size_t end_idx=end)
+    RetTypeOfCalc BuildTreeNow(SegmentTreeNode* root, size_t start_idx, size_t end_idx)
     {
         
         if(start_idx==end_idx)
             return(root->val = BaseCases(Base, start_idx));
-        
         root->left_child = new SegmentTreeNode(root);
         root->right_child = new SegmentTreeNode(root);
-        RetTypeOfCalc val1 = BuildTreeNow(left_child, i*2, start_idx, (start_idx+end_idx)/2);
-        RetTypeOfCalc val2 = BuildTreeNow(right_child, i*2+1, ((start_idx+end_idx)/2)+1, end_idx);
+        RetTypeOfCalc val1 = BuildTreeNow(root->left_child, start_idx, (start_idx+end_idx)/2);
+        RetTypeOfCalc val2 = BuildTreeNow(root->right_child, ((start_idx+end_idx)/2)+1, end_idx);
         if (MergeFunc2 != nullptr)
             return(root->val = MergeFunc2(Base, val1, val2));
         else
             return(root->val = MergeFunc(val1, val2));       
     }
 
-    pair<RetTypeOfCalc,bool> RangeQueryNow(size_t start_idx, size_t end_idx, size_t beg=beg, size_t end=end, SegmentTreeNode* root=root) const
+    pair<RetTypeOfCalc,bool> RangeQueryNow(size_t start_idx, size_t end_idx, size_t beg, size_t end, SegmentTreeNode* root) const
     {
         
-        if(beg == start_idx && end == end_idx)
+        if(beg >= start_idx && end <= end_idx)
             return make_pair(root->val,true);
         
-        if(beg>=end_idx||end<start_idx)
+        if(beg>end_idx||end<start_idx)
             return make_pair(root->val,false);
         
         pair<RetTypeOfCalc,bool> val1 = RangeQueryNow(start_idx, end_idx, beg, (beg+end)/2, root->left_child);
@@ -75,10 +74,10 @@ private :
         if(val1.second&&val2.second)
         {
             if(MergeFunc2!=nullptr){
-                return(make_pair(MergeFunc2(Base, val1, val2),true));
+                return(make_pair(MergeFunc2(Base, val1.first, val2.first),true));
             }
             else{
-                return(make_pair(MergeFunc(val1, val2),true));
+                return(make_pair(MergeFunc(val1.first, val2.first),true));
             }
         }
         
@@ -93,18 +92,18 @@ private :
         }
     }
 
-    void PointUpdateNow(size_t index, size_t range_start=beg, size_t range_end=end, SegmentTreeNode* root=root)
+    void PointUpdateNow(size_t index, size_t range_start, size_t range_end, SegmentTreeNode* root)
     {
+    
         if(range_start==range_end)
         {   
             root->val = BaseCases(Base, index);
             return;
         }
-        
         if(index<=(range_start+range_end)/2)
             PointUpdateNow(index,range_start,(range_start+range_end)/2,root->left_child);
         else
-            PointUpdateNow(index,(range_start+range_end)/2,range_end,root->right_child);
+            PointUpdateNow(index,(range_start+range_end)/2+1,range_end,root->right_child);
         
         if(MergeFunc2!=nullptr)
         {
@@ -119,14 +118,14 @@ private :
 
 public:    
     
-    SegmentTree1d(BaseContainer _Base, RetTypeOfCalc (*_BaseCases)(const BaseContainer&, size_t), RetTypeOfCalc (*_MergeFunc)(RetTypeOfCalc, RetTypeOfCalc), size_t size ,size_t beg_ = 0)
+    SegmentTree1d(BaseContainer _Base, RetTypeOfCalc (*_BaseCases)(const BaseContainer&, size_t), RetTypeOfCalc (*_MergeFunc)(RetTypeOfCalc, RetTypeOfCalc), size_t size ,size_t _beg = 0)
         : Base(_Base), BaseCases(_BaseCases), MergeFunc(_MergeFunc), beg(_beg), end(size-1)
     {
         MergeFunc2 = nullptr;
         root = new SegmentTreeNode; 
     }
     
-    SegmentTree1d(BaseContainer _Base, RetTypeOfCalc (*_BaseCases)(const BaseContainer&, size_t ), RetTypeOfCalc (*_MergeFunc)(const BaseContainer& , RetTypeOfCalc, RetTypeOfCalc), size_t size, size_t beg_ = 0)
+    SegmentTree1d(BaseContainer _Base, RetTypeOfCalc (*_BaseCases)(const BaseContainer&, size_t ), RetTypeOfCalc (*_MergeFunc)(const BaseContainer& , RetTypeOfCalc, RetTypeOfCalc), size_t size, size_t _beg = 0)
         : Base(_Base), BaseCases(_BaseCases), MergeFunc2(_MergeFunc), beg(_beg), end(size-1)
     {
         MergeFunc = nullptr;
@@ -134,28 +133,34 @@ public:
     }
     
     SegmentTree1d& BuildTree(){
-        BuildTreeNow(root);
+        BuildTreeNow(root,beg,end);
         return *this;
     }
 
     RetTypeOfCalc RangeQuery(size_t start_idx, size_t end_idx) const
     {
-        return RangeQueryNow(start_idx, end_idx).first;
+        
+        return RangeQueryNow(start_idx, end_idx, beg, end, root).first;
     }
 
     SegmentTree1d& PointUpdate(size_t index, typename BaseContainer::value_type new_val){
         Base[index] = new_val;
-        PointUpdateNow(index);
+        PointUpdateNow(index, beg, end, root);
     }
 };
 
+int BaseCases(const vector<int>& a, size_t index)
+{return a[index];}
+
+int Merge(int a, int b)
+{return max(a,b);}
+  
 int main(){
-    int BaseCases(vector<int>& a, int index){return a[index];}
-    int Merge(int a, int b){return max(a,b);}
-    vector<int> a  {1,2,3,4,5};
-    SegmentTree<vector<int>,int> st(a,BaseCases,Merge,a.size());
+    vector<int> a  {1,2,9,8,10,5};
+    SegmentTree1d<vector<int>,int> st(a,BaseCases,Merge,a.size()); //Segment Tree to find maximum in any range of array
     st.BuildTree();
     cout<<st.RangeQuery(1,3);
     st.PointUpdate(3,7);
+    cout<<st.RangeQuery(1,3);
 
 }
